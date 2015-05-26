400619b3aa4ac67aa4b6d5b065e6b75d70a8d08b
Devin Jett <djett41@gmail.com>
2015-05-26 03:52:47 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+var karma = require('karma').server;
+var uglify = require('gulp-uglify');
+
+var karmaConf = require('./karma.conf.js');
+var paths = {
+  sass: ['./scss/**/*.scss'],
+  js: ['js/**/*.js'],
+  dist: './dist'
+};
+
+gulp.task('default', ['build']);
+gulp.task('build', ['sass']);
+gulp.task('dist', ['sass', 'scripts']);
+
+gulp.task('scripts', function() {
+  return gulp.src([
+    'js/ionic.filter.bar.js',
+    'js/ionic.filter.bar.directive.js',
+    'js/ionic.filter.bar.config.js',
+    'js/ionic.filter.bar.service.js'
+  ])
+    .pipe(concat('ionic.filter.bar.js'))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.filter.bar.scss')
+    .pipe(gulp.dest(paths.dist))
+    /*
+      Since this is a plugin, we dont want to include ionic scss in dist.  Don't think there is a way to compile scss
+      using ionic vars/mixins without including it in the compiled file.  For now we need to manually remove ionic css
+      after scss is compiled to css
+     */
+    /*
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(minifyCss({ keepSpecialComments: 0 }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest(paths.dist))
+    */
+    .on('end', done);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('karma', function(done) {
+  karmaConf.singleRun = true;
+  karma.start(karmaConf, done);
+});
+
+gulp.task('karma-watch', function(done) {
+  karmaConf.singleRun = false;
+  karma.start(karmaConf, done);
+});
+
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
---
+++ js/ionic.filter.bar.js
@@ -0,0 +1,1 @@
+angular.module('jett.ionic.filter.bar', ['ionic']);
